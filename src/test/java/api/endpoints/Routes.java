e7f8cc424a7b29f58fa6c004eb78c42f8f29678b
Mershelle Rivera <mershellemae@gmail.com>
2025-01-06 11:09:09 -0800

    add routes for the user module

---
+++ src/test/java/api/endpoints/Routes.java
@@ -0,0 +1,13 @@
+package api.endpoints;
+
+/**
+ * This class will maintain the routes of the API
+ */
+public class Routes {
+    public static String base_url = "https://petstore.swagger.io/v2";
+    // User module:
+    public static String post_url = base_url + "/user";
+    public static String get_url = base_url + "/user/{username}";
+    public static String update_url = base_url + "/user/{username}";
+    public static String delete_url = base_url + "/user/{username}";
+}
